4593d068422efd123e0a0c602d62fe1af8c6516a
bikram2github <bikrammaity736@gmail.com>
2026-04-05 09:53:24 +0530

    change in main

--- app/main.py
+++ app/main.py
@@ -1,19 +1,18 @@
-from fastapi import FastAPI,Path,Query,HTTPException
-from fastapi.responses import JSONResponse
-from app.schemas import FinancialRecord,FinancialRecordUpdate
-from app.database import create_db_table, delete_record, insert_record, get_all_records, get_records_by_date, get_by_category, get_by_type,sort_records, update_record,create_users_table
-import uuid
-import json
-
+from fastapi import FastAPI, Path, Query, HTTPException
+from app.schemas import FinancialRecord, FinancialRecordUpdate
+from app.database import create_db_table, create_users_table
+from app.crud import (
+    create_record,
+    get_records,
+    get_records_date,
+    get_records_category,
+    get_records_type,
+    get_sorted,
+    delete,
+    update
+)
 
 app = FastAPI()
-
-
-def load_data():
-    data=get_all_records()
-    data=json.dumps(data)
-
-    return data
 
 
 @app.on_event("startup")
@@ -29,18 +28,18 @@ def root():
 
 
 @app.post("/create")
-def create_record(record: FinancialRecord):
-    data = record.model_dump()
-    data["tran_id"] = str(uuid.uuid4())
-    insert_record(data["tran_id"], data["amount"], data["record_type"], data["category"], data["note"])
-
-    return {"message": "Record created successfully", "record": data}
+def create(record: FinancialRecord):
+    data = create_record(record)
+    return {
+        "message": "Record created successfully",
+        "record": data
+    }
 
 
 
 @app.get("/records")
 def read_records():
-    records = get_all_records()
+    records = get_records()
     if not records:
         raise HTTPException(status_code=404, detail="No records found")
     return {"records": records}
@@ -48,8 +47,10 @@ def read_records():
 
 
 @app.get("/records/date/{date}")
-def read_records_by_date(date: str = Path(..., description="The date of the financial records in YYYY-MM-DD format",examples=["2026-04-04"])):
-    records = get_records_by_date(date)
+def read_records_by_date(
+    date: str = Path(..., description="Format: YYYY-MM-DD", examples=["2026-04-05"])
+):
+    records = get_records_date(date)
     if not records:
         raise HTTPException(status_code=404, detail="No records found for this date")
     return {"records": records}
@@ -57,52 +58,54 @@ def read_records_by_date(date: str = Path(..., description="The date of the fina
 
 
 @app.get("/records/category/{category}")
-def read_records_by_category(category: str = Path(..., description="The category of the financial records",examples=["Rent","Groceries","Education","Entertainment","Utilities","Transportation","Healthcare"])):
-    records = get_by_category(category)
+def read_records_by_category(category: str):
+    records = get_records_category(category)
     if not records:
         raise HTTPException(status_code=404, detail="No records found for this category")
     return {"records": records}
 
 
-
 @app.get("/records/type/{record_type}")
-def read_records_by_type(record_type: str = Path(..., examples=["income", "expense"])):
-    
+def read_records_by_type(record_type: str):
     if record_type not in ["income", "expense"]:
-        raise HTTPException(status_code=400, detail="Invalid type.Should be either 'income' or 'expense'")
+        raise HTTPException(status_code=400, detail="Invalid type.Please provide either 'income' or 'expense'")
 
-    records = get_by_type(record_type)
-
+    records = get_records_type(record_type)
     if not records:
         raise HTTPException(status_code=404, detail="No records found")
 
     return {"records": records}
 
 
-
 @app.get("/sort")
-def read_sorted_records(sort_by: str = Query(..., description="The field to sort by.It should be either 'date' or 'amount'.", examples="date")):
+def read_sorted_records(
+    sort_by: str = Query(..., description="date or amount", examples=["date"])
+):
     if sort_by not in ["date", "amount"]:
-        raise HTTPException(status_code=400, detail="Invalid sort field.Should be either 'date' or 'amount'")
-    sorted_records = sort_records(sort_by)
-    if not sorted_records:
+        raise HTTPException(status_code=400, detail="Invalid sort field")
+
+    records = get_sorted(sort_by)
+    if not records:
         raise HTTPException(status_code=404, detail="No records found")
-    return {"records": sorted_records}
+
+    return {"records": records}
 
 
 @app.delete("/records/{record_id}")
-def delete_record_by_id(record_id: str = Path(..., description="The unique identifier of the financial record to be deleted")):
-    deleted=delete_record(record_id)
+def delete_record_by_id(record_id: str):
+    deleted = delete(record_id)
+
     if not deleted:
-        raise HTTPException(status_code=404, detail="No records found")
-    return {"message": f"Record with id {record_id} has been deleted successfully."}
+        raise HTTPException(status_code=404, detail="Record not found")
+
+    return {"message": f"Record {record_id} deleted successfully"}
 
 
 @app.put("/update/{record_id}")
 def update_record_by_id(record_id: str, record: FinancialRecordUpdate):
-    updated = update_record(record_id, record.model_dump(exclude_unset=True))
+    updated = update(record_id, record.model_dump(exclude_unset=True))
 
     if not updated:
         raise HTTPException(status_code=404, detail="Record not found")
 
-    return {"message": f"Record with id {record_id} has been updated successfully."}
+    return {"message": f"Record {record_id} updated successfully"}
---
+++ app/trail.py
@@ -0,0 +1,108 @@
+from fastapi import FastAPI,Path,Query,HTTPException
+from fastapi.responses import JSONResponse
+from app.schemas import FinancialRecord,FinancialRecordUpdate
+from app.database import create_db_table, delete_record, insert_record, get_all_records, get_records_by_date, get_by_category, get_by_type,sort_records, update_record,create_users_table
+import uuid
+import json
+
+
+app = FastAPI()
+
+
+def load_data():
+    data=get_all_records()
+    data=json.dumps(data)
+
+    return data
+
+
+@app.on_event("startup")
+def startup():
+    create_db_table()
+    create_users_table()
+
+
+@app.get("/")
+def root():
+    return {"message": "Financial Records Management"}
+
+
+
+@app.post("/create")
+def create_record(record: FinancialRecord):
+    data = record.model_dump()
+    data["tran_id"] = str(uuid.uuid4())
+    insert_record(data["tran_id"], data["amount"], data["record_type"], data["category"], data["note"])
+
+    return {"message": "Record created successfully", "record": data}
+
+
+
+@app.get("/records")
+def read_records():
+    records = get_all_records()
+    if not records:
+        raise HTTPException(status_code=404, detail="No records found")
+    return {"records": records}
+
+
+
+@app.get("/records/date/{date}")
+def read_records_by_date(date: str = Path(..., description="The date of the financial records in YYYY-MM-DD format",examples=["2026-04-04"])):
+    records = get_records_by_date(date)
+    if not records:
+        raise HTTPException(status_code=404, detail="No records found for this date")
+    return {"records": records}
+
+
+
+@app.get("/records/category/{category}")
+def read_records_by_category(category: str = Path(..., description="The category of the financial records",examples=["Rent","Groceries","Education","Entertainment","Utilities","Transportation","Healthcare"])):
+    records = get_by_category(category)
+    if not records:
+        raise HTTPException(status_code=404, detail="No records found for this category")
+    return {"records": records}
+
+
+
+@app.get("/records/type/{record_type}")
+def read_records_by_type(record_type: str = Path(..., examples=["income", "expense"])):
+    
+    if record_type not in ["income", "expense"]:
+        raise HTTPException(status_code=400, detail="Invalid type.Should be either 'income' or 'expense'")
+
+    records = get_by_type(record_type)
+
+    if not records:
+        raise HTTPException(status_code=404, detail="No records found")
+
+    return {"records": records}
+
+
+
+@app.get("/sort")
+def read_sorted_records(sort_by: str = Query(..., description="The field to sort by.It should be either 'date' or 'amount'.", examples="date")):
+    if sort_by not in ["date", "amount"]:
+        raise HTTPException(status_code=400, detail="Invalid sort field.Should be either 'date' or 'amount'")
+    sorted_records = sort_records(sort_by)
+    if not sorted_records:
+        raise HTTPException(status_code=404, detail="No records found")
+    return {"records": sorted_records}
+
+
+@app.delete("/records/{record_id}")
+def delete_record_by_id(record_id: str = Path(..., description="The unique identifier of the financial record to be deleted")):
+    deleted=delete_record(record_id)
+    if not deleted:
+        raise HTTPException(status_code=404, detail="No records found")
+    return {"message": f"Record with id {record_id} has been deleted successfully."}
+
+
+@app.put("/update/{record_id}")
+def update_record_by_id(record_id: str, record: FinancialRecordUpdate):
+    updated = update_record(record_id, record.model_dump(exclude_unset=True))
+
+    if not updated:
+        raise HTTPException(status_code=404, detail="Record not found")
+
+    return {"message": f"Record with id {record_id} has been updated successfully."}
